fd3d56635cc5d18f45f0f947fbac57e6fadba594
Degeq <gorlovoy89@mail.ru>
2023-07-25 21:10:01 +0300

    PatternsHW3 Commit 1

---
+++ ItterableRandom/src/main/java/Randoms.java
@@ -0,0 +1,39 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer>{
+
+    protected Random random;
+    protected int min, max;
+
+    public Randoms(int min, int max) {
+        if (min > max) {
+            throw new IllegalArgumentException("Вы ввели неверный диапазон (минимум должен быть > максимума");
+        } else {
+            this.random = new Random();
+            this.max = max;
+            this.min = min;
+        }
+
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+
+        return new Iterator<Integer>() {
+            int next;
+            boolean hasNext;
+
+            @Override
+            public boolean hasNext() {
+                next = min + random.nextInt(max-min + 1);
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return next;
+            }
+        };
+    }
+}
